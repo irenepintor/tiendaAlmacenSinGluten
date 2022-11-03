06cba7a179d8b6dd69e2759746b9129aa6577ea3
irenepintor <pintor.irene@outlook.com>
2022-11-03 01:31:03 -0300

    reEntrega

--- js/index.js
+++ js/index.js
@@ -1,44 +1,86 @@
-let compra = prompt("Bienvenido a Almacén Sin Gluten. ¿Desea efectuar una compra? Si o No")
+const edad = 18;
 
-if (compra === "Si" || compra === "si") {
-    alert("Procede a elegir tus productos")
-}else if (compra === "No" || compra === "no") {
-    alert("Lamentamos que no te interesen nuestros productos. Hasta la proxima.")
-}else(alert("Ingresa una opción valida. Recarga la pestaña."))
+const saludo = alert ("¡Bienvenido a Almacen Sin Gluten!");
+let edadIngresada = parseInt( prompt ("Ingrese su edad:"));
+let repetir = true;
 
-if (compra === "Si" || compra === "si") {
-    let carritoDeCompras = prompt("Elegí de nuestra lista de Productos: \1.Pan Baguette: $230 \2.Red Velvet: $4180 \3.Croissants: $290 \4.Pan de Ají: $410 \5.Scons de Queso: $410 \6.Cheesecake: $5180")
-    switch (carritoDeCompras) {
-        case "1":
-            alert("Compraste Pan Banguette")
-            compra += 230;
-        break;
-        case "2":
-            alert("Compraste Red Velvet")
-            compra += 4180;
-        break;
-        case "3":
-            alert("Compraste Croissants")
-            compra += 290;
-        break;
-        case "4":
-            alert("Compraste Pan de Ají")
-            compra += 410;
-        break;
-        case "5":
-            alert("Compraste Scons de Queso")
-            compra += 410;
-        break;
-        case "6":
-            alert("Compraste Cheesecake")
-            compra += 5180;
-        break;    
-        default:
-            alert("Elije un producto de la lista por favor.")
-        break;
+function controlDeEdad (edadIngresada) {
+    return (18 <= edadIngresada)
+};
+if (controlDeEdad(edadIngresada)) {
+    alert ("Ahora sí puedes comenzar a cargar el carrito.");
+    repetir = false;
+}
+while (repetir){
+    if(isNaN(edadIngresada)){
+        edadIngresada = parseInt(prompt("Por favor, ingrese un número válido."));
+        continue
+    }else if(controlDeEdad(edadIngresada)) {
+        alert ("Ahora sí puedes comenzar a cargar el carrito.");
+        repetir = false;
+    }else{ 
+        alert ("Eres menor de edad. No te podemos dejar comprar sin permiso de un adulto.");
+        repetir = false;
     }
-    carritoDeCompras = prompt("Por si necesitas algo más: \1.Pan Baguette: $230 \2.Red Velvet: $4180 \3.Croissants: $290 \4.Pan de Ají: $410 \5.Scons de Queso: $410 \6.Cheesecake: $5180")
-}else{
-    alert("Tú compra es de $ " + compra);
-    alert("Gracias por tú compra. Vuelve pronto.")
-} 
+}
+
+
+
+
+// const panBaguette = 230;
+// const redVelvet = 4180;
+// const croissant = 290;
+
+// const promoUno = panBaguette + croissant;
+// const promoDos = redVelvet + croissant;
+// const promoTres = panBaguette + redVelvet + croissant;
+
+//function = costoTotal
+//Parametros de la function = (panBaguette,redVelvet,croissant,costo) => para que funcione la function tienen que cumplirse todos los parametros
+//Por prompt elejir los productos y por switch que se vayan sumando:
+//Por prompt elejir el medio de pago, segun lo elejido habra recargos o no
+//Segun la suma de productos por switch y el medio de pago elejido aplicar recargos o descuentos
+
+//     let carrito = 0;
+//         let productos = prompt("Elige tus productos: \n1.Pan Baguette\n2.Red Velvet\n3.Croissant")
+//         switch (productos) {
+//             case "1":
+//                 alert(`Elegiste Pan Baguette $${panBaguette}`);
+//                 carrito += panBaguette;
+//                 break;
+//             case "2":
+//                 alert(`Elegiste Red Velvet $${redVelvet}`);
+//                 carrito += redVelvet;
+//                 break;
+//             case "3":
+//                 alert(`Elegiste Croissant $${croissant}`);
+//                 carrito += croissant;
+//                 break;
+//             default:
+//                 alert("Elige una opción válida.")
+//                 break;
+//         }
+
+
+//     let medioDePago = prompt("Elige tú medio de pago: \n1.Efectivo: descuento del 10%\n2.Debito: recargo del 10%\n3.Credito: recargo del 20%\n4.Transferencia: recargo del 5%")
+//         switch (medioDePago) {
+//             case "1":
+//                 alert("Elegiste pago en Efectivo");
+//                 carrito * 0.9;
+//                 break;
+//             case "2":
+//                 alert("Elegiste pago con Debito");
+//                 carrito * 1.10;
+//                 break;
+//             case "3":
+//                 alert("Elegiste pago con Credito");
+//                 carrito * 1.2;
+//                 break;
+//             case "4":
+//                 alert("Elegiste Transferencia")
+//                 carrito * 1.05
+//                 break;
+//             default:
+//                 alert("Elige una opción válida.")
+//                 break;
+//         }
